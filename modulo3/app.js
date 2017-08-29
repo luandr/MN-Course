b6f01184c4cb0e3b14f532fc782b88537ce0169c
luandr <stringigualanull@outlook.com>
2017-08-29 12:51:10 -0300

    Consign  implementado
    Facilitar o routing

--- modulo3/app.js
+++ modulo3/app.js
@@ -1,16 +1,16 @@
 //cfg app
 var app = require('./config/server');
-//routes
-var rotaNoticias = require('./app/routes/noticias');
+
+
+//routes old
+/* var rotaNoticias = require('./app/routes/noticias');
 rotaNoticias(app); //pode ser feito assim
 var rotaHome = require('./app/routes/home')(app);//ou assim
-var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app); */
+
 //vars
 //seta a porta como variavel
 var port = 8080;
-
-
-
 
 
 //escuta a porta 'port'
