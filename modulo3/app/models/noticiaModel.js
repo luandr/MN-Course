5f6131d122c98c43ee2dedbeaf34168722fb9d05
luandr <stringigualanull@outlook.com>
2017-08-30 13:20:05 -0300

    Algumas coisas

--- modulo3/app/models/noticiaModel.js
+++ modulo3/app/models/noticiaModel.js
@@ -1,6 +1,10 @@
+
 module.exports = () => {
     this.getNoticias = (conn, callback) => {
         conn.query('select id, titulo, noticia from noticias', callback);
+    }
+    this.salvarNoticia = (obj, conn, callback) => {
+        conn.query('insert into noticias set ?', obj, callback);
     }
     return this;
 }
--- modulo3/app/routes/admin.js
+++ modulo3/app/routes/admin.js
@@ -4,8 +4,16 @@ module.exports = (app) => {
         res.render('admin/form_add_noticia');
     });
     app.post('/noticias/salvar', (req, res) => {
-        var noticias = req.body;
-        console.log(noticias);
-        res.render('admin/form_add_noticia');
+        var conn = app.config.dbConn();
+        var noticiaModel = app.app.models.noticiaModel;
+        var noticia = req.body;
+
+        noticiaModel.salvarNoticia(noticia, conn, (err, result) => {
+            if (err) {
+                res.render('noticias/noticias', { error: err });
+            }
+            res.redirect('/noticias');
+        });
+
     });
 }
